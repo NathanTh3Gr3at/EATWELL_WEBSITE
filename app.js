e1e11866fd19152bd39f8ccc258d975c7cb72546
NathanTh3Gr3at <112683403+NathanTh3Gr3at@users.noreply.github.com>
2025-02-08 14:03:23 +0200

    Refactor routing and add restaurant detail view; implement error handling and UUID for restaurant IDs

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 // npm start
-const fs = require("fs");
 const path = require("path");
+
 const express = require("express");
+
+const defaultRoutes = require("./routes/default");
+const restaurantRoutes = require("./routes/restaurants");
 
 const app = express();
 
@@ -11,40 +14,19 @@ app.set("view engine", "ejs");
 app.use(express.static("public"));
 app.use(express.urlencoded({ extended: false }));
 
-app.get("/", function (req, res) {
-  res.render("index");
+// Defaults
+app.use("/", defaultRoutes);
+
+// Restaurants
+app.use("/", restaurantRoutes);
+
+// Errors
+app.use(function (req, res) {
+  res.status(404).render("404");
+});
+app.use(function (error, req, res, next) {
+  res.status(500).render("500");
 });
 
-app.get("/restaurants", function (req, res) {
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-  res.render("restaurants", {
-    numberOfRestaurants: storedRestaurants.length,
-    restaurants: storedRestaurants,
-  });
-});
-
-app.get("/confirm", function (req, res) {
-  res.render("confirm");
-});
-
-app.get("/about", function (req, res) {
-  res.render("about");
-});
-
-app.get("/recommend", function (req, res) {
-  res.render("recommend");
-});
-app.post("/recommend", function (req, res) {
-  const restaurant = req.body;
-  const filePath = path.join(__dirname, "data", "restaurants.json");
-  const fileData = fs.readFileSync(filePath);
-  const storedRestaurants = JSON.parse(fileData);
-
-  storedRestaurants.push(restaurant);
-  fs.writeFileSync(filePath, JSON.stringify(storedRestaurants));
-  res.redirect("/confirm");
-});
-
+// Listen
 app.listen(3000);
